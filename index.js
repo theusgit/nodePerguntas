f511be275d6acc170a641480ae52a022722bf635
theusgit <theusgit@outlook.com>
2021-01-08 17:24:06 -0300

    Salvando perguntas no banco de dados

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require('./database/database');
-const perguntaModel=require('./database/Pergunta');
+const Pergunta=require('./database/Pergunta');
 
 //Database
 connection

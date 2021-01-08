56cfa0579ab65194cd86e72e63f6c95a02dcae9a
theusgit <theusgit@outlook.com>
2021-01-07 21:55:44 -0300

    Envio de formulário body-parser

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -8,10 +13,24 @@ app.get("/", (req, res) => {
     res.render("index")
 });
 
-app.get("/perguntar",(req,res)=>{
+app.get("/perguntar", (req, res) => {
     res.render("perguntar")
 });
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido titulo " + titulo + " " + "descricao " + descricao);
+})
+
+
 
 app.listen(4040, () => {
     console.log("app rodando")
 });
+
+
+
+
+
+
